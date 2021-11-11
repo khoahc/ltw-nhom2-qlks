30a89eeedefd3dbf62db73641115349b6687cbad
Nguyen36 <88275484+Nguyen36@users.noreply.github.com>
2021-11-11 20:33:20 +0700

    Update Phong.java
    beans phong

--- src/main/java/beans/Phong.java
+++ src/main/java/beans/Phong.java
@@ -1,5 +1,32 @@
 package beans;
 
 public class Phong {
-
+	private int id_phong;
+	private String ten_phong;
+	private int id_loai_phong;
+	private int id_trang_thai;
+	public int getId_phong() {
+		return id_phong;
+	}
+	public void setId_phong(int id_phong) {
+		this.id_phong = id_phong;
+	}
+	public String getTen_phong() {
+		return ten_phong;
+	}
+	public void setTen_phong(String ten_phong) {
+		this.ten_phong = ten_phong;
+	}
+	public int getId_loai_phong() {
+		return id_loai_phong;
+	}
+	public void setId_loai_phong(int id_loai_phong) {
+		this.id_loai_phong = id_loai_phong;
+	}
+	public int getId_trang_thai() {
+		return id_trang_thai;
+	}
+	public void setId_trang_thai(int id_trang_thai) {
+		this.id_trang_thai = id_trang_thai;
+	}
 }
