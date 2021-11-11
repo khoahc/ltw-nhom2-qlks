e519d366ea046b6ab26f5665db818fbea4968568
Nguyen36 <88275484+Nguyen36@users.noreply.github.com>
2021-11-11 20:33:28 +0700

    Update Quyen.java
    beans quyen

--- src/main/java/beans/Quyen.java
+++ src/main/java/beans/Quyen.java
@@ -1,5 +1,19 @@
 package beans;
 
 public class Quyen {
-
+	private int id_quyen;
+	private String ten_quyen;
+	public int getId_quyen() {
+		return id_quyen;
+	}
+	public void setId_quyen(int id_quyen) {
+		this.id_quyen = id_quyen;
+	}
+	public String getTen_quyen() {
+		return ten_quyen;
+	}
+	public void setTen_quyen(String ten_quyen) {
+		this.ten_quyen = ten_quyen;
+	}
+	
 }
