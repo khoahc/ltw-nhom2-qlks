eb65ff36f85ee811e74b2839af8eea4e841813c4
Nguyen36 <88275484+Nguyen36@users.noreply.github.com>
2021-11-11 20:32:48 +0700

    Update KhachHang.java
    bean khachhang

--- src/main/java/beans/KhachHang.java
+++ src/main/java/beans/KhachHang.java
@@ -1,5 +1,39 @@
 package beans;
 
 public class KhachHang {
-
+	private int id_khach;
+	private String ho_ten;
+	private String cmnd;
+	private String dia_chi;
+	private int id_booking;
+	public int getId_khach() {
+		return id_khach;
+	}
+	public void setId_khach(int id_khach) {
+		this.id_khach = id_khach;
+	}
+	public String getHo_ten() {
+		return ho_ten;
+	}
+	public void setHo_ten(String ho_ten) {
+		this.ho_ten = ho_ten;
+	}
+	public String getCmnd() {
+		return cmnd;
+	}
+	public void setCmnd(String cmnd) {
+		this.cmnd = cmnd;
+	}
+	public String getDia_chi() {
+		return dia_chi;
+	}
+	public void setDia_chi(String dia_chi) {
+		this.dia_chi = dia_chi;
+	}
+	public int getId_booking() {
+		return id_booking;
+	}
+	public void setId_booking(int id_booking) {
+		this.id_booking = id_booking;
+	}
 }
